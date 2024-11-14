18851827638658cbcae3081395801b4f4fc86478
Y Rajasekhar Reddy <sekharan7@gmail.com>
2024-11-14 13:05:18 +0530

    first commit

---
+++ sparktest.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+# Initialize Spark session
+spark = SparkSession.builder \
+    .appName("DatabricksLocalPython") \
+    .master("local[*]") \
+    .getOrCreate()
+
+# Example of using Spark to read a JSON file and show the data
+df = spark.read.option("multiLine", "true").json("people.json")
+
+# Flatten the 'people' array using explode
+df_exploded = df.select(F.explode(df.people).alias("person"))
+
+# Now, df_exploded will contain each person as a separate row
+df_exploded.show(truncate=False)
+
+# Access fields in the nested JSON structure
+df_exploded.select("person.name", "person.age", "person.city").show(truncate=False)
+
+
+# Stop the Spark session
+spark.stop()
